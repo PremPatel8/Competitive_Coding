2b70f35387bed1e25b35137dc594242a204975d3
Prem Patel <prem.patel971@gmail.com>
2020-05-25 19:54:07 -0500

    My Sol

---
+++ HackerRank/HackerRank Trees- Is This a Binary Search Tree.py
@@ -0,0 +1,32 @@
+from binarytree import tree, bst
+
+
+def checkBST(root):
+    nodes = []
+    
+    def InOrder(node):
+        if node.left:
+            InOrder(node.left)
+        
+        nodes.append(node.val)
+
+        if node.right:
+            InOrder(node.right)
+
+    InOrder(root)
+
+    uniqueNodes = set(nodes)
+
+    if len(nodes) == len(uniqueNodes):
+        if sorted(nodes) == nodes:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+tree = bst(is_perfect=True)
+
+print(tree)
+
+print(checkBST(tree))
