537eba0b4f08d252ddbba9306bcbe2fedf6cfccc
Prem Patel <prem.patel971@gmail.com>
2020-06-05 15:55:23 -0500

    Incomplete

--- HackerRank/Graph Matrix.py
+++ HackerRank/Graph Matrix.py
@@ -8,11 +8,49 @@ import sys
 from collections import defaultdict
 # Complete the minTime function below.
 
+
 class Disjoint_Set():
-    parent = defaultdict()
+
+    def __init__(self):
+        self.parent = defaultdict(int)
+
+    def make_set(self, city, is_machine):
+        self.parent[city] = is_machine
+
+    def Find(self, city):
+        if self.parent[city] == city:
+            return city
+        else:
+            return self.Find(self.parent[city])
+
+    def Union(self, set1, set2):
+        self.parent[set2] = self.parent[set1]
+
+    def has_machine(self, city):
+        return self.parent[city]
+
 
 def minTime(roads, machines):
+    minTime = 0
     roads.sort(key=lambda x: x[2], reverse=True)
+
+    city_sets = Disjoint_Set()
+
+    # print(roads)
+    # print(sorted(machines))
+
+    for m in machines:
+        city_sets.make_set(m, True)
+
+    # print(city_sets.parent)
+
+    for c1, c2, time in roads:
+        if city_sets.has_machine(c1) and city_sets.has_machine(c2):
+            minTime += time
+        else:
+            city_sets.Union(c1, c2)
+
+    return minTime
 
 
 if __name__ == '__main__':
