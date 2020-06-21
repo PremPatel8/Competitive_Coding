b2c30bf4772a4fe0ac8a6573991fc15b77fd7742
PremPatel8 <PremPatel_8@outlook.com>
2020-06-21 00:30:49 -0500

    Add 2 solutions one using set and one using dict

--- LeetCode/5441. Making File Names Unique.py
+++ LeetCode/5441. Making File Names Unique.py
@@ -2,11 +2,11 @@ from collections import OrderedDict
 from collections import defaultdict
 from typing import List
 
-
+# Runtime: 420 ms
+# Memory Usage: 28.9 MB
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
-        ans = []
-        unique_names = set()
+        unique_names = OrderedDict()
         postfix_val = defaultdict(lambda: 1)
 
         for name in names:
@@ -21,14 +21,33 @@ class Solution:
 
                     postfix_val[name] += 1
 
-            ans.append(name_to_add)
-            unique_names.add(name_to_add)
+            unique_names[name_to_add] = None
 
-        return ans
+        return list(unique_names)
 
 
-myClass = Solution()
 
-names = ["kaido", "kaido(1)", "kaido", "kaido(1)"]
+# # Runtime: 428 ms
+# # Memory Usage: 25.5 MB
+# class Solution:
+#     def getFolderNames(self, names: List[str]) -> List[str]:
+#         ans = []
+#         unique_names = set()
+#         postfix_val = defaultdict(lambda: 1)
 
-print(myClass.getFolderNames(names))
+#         for name in names:
+#             if name not in unique_names:
+#                 name_to_add = name
+#             else:
+#                 while True:
+#                     name_to_add = "%s(%s)" % (name, postfix_val[name])
+
+#                     if name_to_add not in unique_names:
+#                         break
+
+#                     postfix_val[name] += 1
+
+#             ans.append(name_to_add)
+#             unique_names.add(name_to_add)
+
+#         return ans
