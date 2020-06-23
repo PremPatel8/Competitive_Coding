8e14cca20ac8a363ec21d57460370eaeb6c0b910
PremPatel8 <PremPatel_8@outlook.com>
2020-06-22 22:09:45 -0500

    My alt solution

--- LeetCode/Set Matrix Zeroes.py
+++ LeetCode/Set Matrix Zeroes.py
@@ -2,10 +2,13 @@ from typing import List
 
 """ Runtime: 128 ms
 Memory Usage: 14.4 MB """
+
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         zeroLocations = []
-        indexSeen = dict()
+        rSeen = set()
+        cSeen = set()
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
@@ -13,30 +16,26 @@ class Solution:
                     zeroLocations.append([i, j])
 
         for r, c in zeroLocations:
-            row = "R"+str(r)
-            col = "C"+str(c)
+            if r not in rSeen:
+                rSeen.add(r)
+                for col in range(len(matrix[0])):
+                    matrix[r][col] = 0
+            if c not in cSeen:
+                cSeen.add(c)
+                for row in range(len(matrix)):
+                    matrix[row][c] = 0
 
-            if row not in indexSeen:
-                indexSeen[row] = None
-                matrix[r] = [0]*len(matrix[0])
-
-            if col not in indexSeen:
-                indexSeen[col] = None
-
-                for r in range(len(matrix)):
-                    matrix[r][c] = 0
-        
         # print(matrix)
-        # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
 
 
 myClassObj = Solution()
 
 # inpt = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 inpt = [
-  [0,1,2,0],
-  [3,4,5,2],
-  [1,3,1,5]
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5]
 ]
 
 myClassObj.setZeroes(inpt)
