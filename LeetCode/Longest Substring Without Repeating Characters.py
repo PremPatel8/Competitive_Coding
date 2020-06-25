2e7511eebb17330d206171cab6e05e85576307bd
PremPatel8 <PremPatel_8@outlook.com>
2020-06-24 19:45:37 -0500

    LeetCode Optimized solution using char array

--- LeetCode/Longest Substring Without Repeating Characters.py
+++ LeetCode/Longest Substring Without Repeating Characters.py
@@ -1,21 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ptr1 = ptr2 = 0
-        uniqueChr = set()
+        strLen = len(s)
         maxoutput = 0
+        ptr1 = ptr2 = 0
+        index = [0]*26
 
-        while ptr2 < len(s):
-            if s[ptr2] not in uniqueChr:
-                uniqueChr.add(s[ptr2])
-                ptr2+=1
-                maxoutput = max(maxoutput, len(uniqueChr))
-            else:
-                uniqueChr.remove(s[ptr1])
-                ptr1+=1
-        
+        while ptr2 < strLen:
+            ptr1 = max(ptr1, index[ord(s[ptr2])-ord('a')])
+
+            maxoutput = max(maxoutput, ptr2-ptr1+1)
+
+            index[ord(s[ptr2])-ord('a')] = ptr2+1
+
+            ptr2 += 1
+
         return maxoutput
-
-
 
 
 myObj = Solution()
