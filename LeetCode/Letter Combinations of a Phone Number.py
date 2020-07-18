b0f6e0f869e47c05f1cebe051b8aaa38d47b56ef
PremPatel8 <PremPatel_8@outlook.com>
2020-07-18 13:38:29 -0500

    LeetCode official solution, backtracking approach

--- LeetCode/Letter Combinations of a Phone Number.py
+++ LeetCode/Letter Combinations of a Phone Number.py
@@ -19,40 +19,50 @@ Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 """
 
-# Nick White Solution, Queue based approach
+# LeetCode official solution, backtracking based approach
+"""
+25 / 25 test cases passed.
+Status: Accepted
+Runtime: 32 ms
+Memory Usage: 13.8 MB
+"""
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        output = deque()
-        if not len(digits):
-            return output
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
 
-        output.append("")
+        def backtrack(combination, next_digits):
+            # if there is no more digits to check
+            if len(next_digits) == 0:
+                # the combination is done
+                output.append(combination)
+            # if there are still digits to check
+            else:
+                # iterate over all letters which map
+                # the next available digit
+                for letter in phone[next_digits[0]]:
+                    # append the current letter to the combination
+                    # and proceed to the next digits
+                    backtrack(combination + letter, next_digits[1:])
 
-        char_map = ['0', '1', "abc", "def", "ghi",
-                    "jkl", "mno", "pqrs", "tuv", "wxyz"]
-
-        i = 0
-
-        while(i < len(digits)):
-            index = int(digits[i])
-
-            while len(output[0]) == i:
-                permutation = output.popleft()
-
-                for ch in char_map[index]:
-                    output.append(permutation+ch)
-
-            i += 1
-
+        output = []
+        if digits:
+            backtrack("", digits)
         return output
 
 
 myobj = Solution()
 
 print(myobj.letterCombinations("23"))
-
-#%%
-digit = "3"
-digit[1:]
