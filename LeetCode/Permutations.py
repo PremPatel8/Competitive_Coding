930bb39747f5e73d0d5ed004e18c214f0adde634
PremPatel8 <PremPatel_8@outlook.com>
2020-07-19 16:18:12 -0500

    DFS Solution for permutation

--- LeetCode/Permutations.py
+++ LeetCode/Permutations.py
@@ -21,22 +21,45 @@ Output:
 ]
 """
 
-
+# DFS Solution
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return []
-        if len(nums) == 1:
-            return [nums]
-        l = []
-        for i in range(len(nums)):
-            m = nums[i]
-            rem = nums[:i]+nums[i+1:]
-            for p in self.permute(rem):
-                l.append([m]+p)
-        return l
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+
+        def backtrack(temp, nums, output):
+            if not nums:
+                output.append(temp)
+
+            for i in range(len(nums)):
+                new_nums = nums[:i]+nums[i+1:]
+                backtrack(temp+[nums[i]], new_nums, output)
+
+        if nums:
+            backtrack([], nums, output)
+
+        return output
 
 
 myobj = Solution()
 
-print(myobj.permute([1, 2, 3]))
+print(myobj.subsets([1, 2, 3]))
+
+
+""" 
+dfs(nums = [1, 2, 3] , path = [] , result = [] )
+|____ dfs(nums = [2, 3] , path = [1] , result = [] )
+|      |___dfs(nums = [3] , path = [1, 2] , result = [] )
+|      |    |___dfs(nums = [] , path = [1, 2, 3] , result = [[1, 2, 3]] ) # added a new permutation to the result
+|      |___dfs(nums = [2] , path = [1, 3] , result = [[1, 2, 3]] )
+|           |___dfs(nums = [] , path = [1, 3, 2] , result = [[1, 2, 3], [1, 3, 2]] ) # added a new permutation to the result
+|____ dfs(nums = [1, 3] , path = [2] , result = [[1, 2, 3], [1, 3, 2]] )
+|      |___dfs(nums = [3] , path = [2, 1] , result = [[1, 2, 3], [1, 3, 2]] )
+|      |    |___dfs(nums = [] , path = [2, 1, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] ) # added a new permutation to the result
+|      |___dfs(nums = [1] , path = [2, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] )
+|           |___dfs(nums = [] , path = [2, 3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] ) # added a new permutation to the result
+|____ dfs(nums = [1, 2] , path = [3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+       |___dfs(nums = [2] , path = [3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+       |    |___dfs(nums = [] , path = [3, 1, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] ) # added a new permutation to the result
+       |___dfs(nums = [1] , path = [3, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] )
+            |___dfs(nums = [] , path = [3, 2, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] ) # added a new permutation to the result 
+"""
