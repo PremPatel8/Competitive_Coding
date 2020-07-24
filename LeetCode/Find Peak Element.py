fd33dc9dea94ce245665b8f8858af73fb362318e
PremPatel8 <PremPatel_8@outlook.com>
2020-07-24 04:47:21 -0500

    Iterative Binary Search Time complexity : O(log n)

--- LeetCode/Find Peak Element.py
+++ LeetCode/Find Peak Element.py
@@ -26,24 +26,44 @@ Follow up: Your solution should be in logarithmic complexity.
 
 """ 59 / 59 test cases passed.
 	Status: Accepted
-Runtime: 52 ms
-Memory Usage: 13.8 MB """
+Runtime: 48 ms
+Memory Usage: 14.1 MB """
 
-# Time complexity : O(n), Space complexity : O(1) Linear solution
+# Time complexity : O(log n), Space complexity : O(1) Iterative Binary Search solution
 
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if not nums or len(nums) == 1:
-            return 0
+        left = 0
+        right = len(nums)-1
 
-        for i in range(len(nums)-1):
-            if nums[i] > nums[i+1]:
-                return i
+        while left < right:
+            mid = (left+right)//2
+            if nums[mid] > nums[mid+1]:
+                right = mid
+            else:
+                left = mid+1
 
-        return len(nums)-1
+        return left
 
 
 myobj = Solution()
 inpt = [1, 2]
 print(myobj.findPeakElement(inpt))
+
+
+""" Recursive Binary Search
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        return search(nums, mid + 1, r);
+    }
+}
+ """
