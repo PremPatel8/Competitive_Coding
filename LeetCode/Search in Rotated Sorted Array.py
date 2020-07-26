f5c576d56be4a5d939c7feb4b5c5e5f96155d63b
PremPatel8 <PremPatel_8@outlook.com>
2020-07-25 21:01:48 -0500

    Binary search solution without infinity

--- LeetCode/Search in Rotated Sorted Array.py
+++ LeetCode/Search in Rotated Sorted Array.py
@@ -23,43 +23,59 @@ Output: -1
 
 """ 196 / 196 test cases passed.
 	Status: Accepted
-Runtime: 48 ms
-Memory Usage: 14 MB """
+Runtime: 60 ms
+Memory Usage: 13.9 MB """
 
-# Time complexity : O(n log⁡ n) Space complexity : O(1) Binary search solution with infinity
+# Time complexity : O(log⁡ n) Space complexity : O(1) Binary search solution without infinity
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums, target):
         left = 0
         right = len(nums)-1
 
         while left <= right:
             mid = (right-left)//2 + left
 
-            comparator = nums[mid]
-
-            if ((target >= nums[0]) == (nums[mid] >= nums[0])):
-                comparator = nums[mid]
-            elif target < nums[0]:
-                comparator = -math.inf
-            else:
-                comparator = math.inf
-
-            if comparator == target:
+            # if found target value, return the index
+            if nums[mid] == target:
                 return mid
 
-            if target > comparator:
-                left = mid+1
-            else:
-                right = mid-1
-
+            # determine it's left rotated or right rotated
+            """
+            No rotated:
+            1 2 3 4 5 6 7
+                 mid
+                 
+            left rotated: pivot at the left side of the origin sorted array, A[mid] >= A[left]
+            3 4 5 6 7 1 2
+                 mid
+            search in A[left] ~ A [mid] if A[left] <= target < A[mid] else, search right side
+            
+            right rotated: pivot at the right side of the origin sorted array, A[mid] < A[left]
+            6 7 1 2 3 4 5
+                 mid          
+            search in A[mid] ~ A[right] if A[mid] < target <= A[right] else, search left side
+            """
+            if nums[mid] >= nums[left]:  # left rotated
+                # in ascending order side
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:  # right rotated
+                # in ascending order side
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        # cannot find the target value
         return -1
 
 
 myobj = Solution()
 # inpt = [4, 5, 6, 7, 0, 1, 2]
 # target = 0
-inpt = [1,3]
+inpt = [1, 3]
 target = 3
 print(myobj.search(inpt, target))
