e55773c42bdf5a80184b83eedf0f5246d3233d13
PremPatel8 <PremPatel_8@outlook.com>
2020-07-27 21:10:38 -0500

    Math solution

--- LeetCode/Unique Paths.py
+++ LeetCode/Unique Paths.py
@@ -1,4 +1,5 @@
 from typing import List
+import math
 
 """
 Problem Name: Unique Paths
@@ -30,23 +31,23 @@ It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
 Resources:
 https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/808/discuss/22954/C++-DP
 https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/808/discuss/23234/Accpeted-simple-Python-DP-solution.
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/808/discuss/22975/Python-easy-to-understand-solutions-(math-dp-O(m*n)-and-O(n)-space).
 """
 
 """ 62 / 62 test cases passed.
 	Status: Accepted
-Runtime: 36 ms
-Memory Usage: 14.1 MB """
+Runtime: 60 ms
+Memory Usage: 13.8 MB """
 
-# Time complexity : O(m*n) Space complexity : O(n) Optimized DP solution
+# Time complexity : O() Space complexity : O() Math Permutation Factorial solution
 
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [1] * n
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[j] = dp[j - 1] + dp[j]
-        return dp[-1] if m and n else 0
+        if not m or not n:
+            return 0
+
+        return math.factorial((m-1)+(n-1))//(math.factorial(m-1)*math.factorial(n-1))
 
 
 myobj = Solution()
