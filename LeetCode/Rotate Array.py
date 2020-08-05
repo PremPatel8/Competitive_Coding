028768c187b863b63b9c4c5a8a038394005d1850
PremPatel8 <PremPatel_8@outlook.com>
2020-08-04 23:54:09 -0500

    Using Reverse

--- LeetCode/Rotate Array.py
+++ LeetCode/Rotate Array.py
@@ -34,33 +34,22 @@ Resources:
 
 # Solution techniques are Using Extra Array and Using Cyclic Replacements and using list reverse
 
-# Time complexity : O(n) Space complexity : O(n) Using Cyclic Replacements
+# Time complexity : O(n) Space complexity : O(n) Using Reverse
 
 
 class Solution:
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
         k %= n
 
-        start = count = 0
-        
-        while count < n:
-            current = start
-            prev = nums[start]
-
-            while True:
-                next_idx = (current + k) % n
-
-                nums[next_idx], prev = prev, nums[next_idx]
-
-                current = next_idx
-
-                count += 1
-
-                if start == current:
-                    break
-
-            start += 1
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
 
 
 myobj = Solution()
