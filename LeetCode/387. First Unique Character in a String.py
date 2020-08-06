a0edb1d1b856b07c65f7ca5e56ded6a7d217e2bb
PremPatel8 <PremPatel_8@outlook.com>
2020-08-05 22:08:56 -0500

    Comments added

--- LeetCode/387. First Unique Character in a String.py
+++ LeetCode/387. First Unique Character in a String.py
@@ -1,5 +1,5 @@
 from typing import List
-from collections import OrderedDict
+from collections import Counter, OrderedDict
 
 """
 Problem Name: 387. First Unique Character in a String
@@ -19,7 +19,8 @@ return 2.
 Note: You may assume the string contains only lowercase English letters.
 
 Resources:
-
+https://stackoverflow.com/questions/23747564/how-to-get-count-dict-of-items-but-maintain-the-order-in-which-they-appear
+https://docs.python.org/3.3/library/collections.html#ordereddict-examples-and-recipes
 """
 
 """ 
@@ -27,9 +28,22 @@ runtime: 200 ms
 Memory: 13.6 MB
  """
 
-# Solution techniques are
+""" Solution techniques are iterate through the string once to get the count of all chars and
+then iterate through the string again and return the position of the first char whose count is 1
+Or my solution use some kind of ordered dict data structure to keep the count of all chars and their 1st encountered positions
+and then iterate through this data structure and return the position of the first char whose count is 1 """
 
-# Time complexity : O(n) Space complexity : O(n) Using OrderedDict
+# Time complexity : O(n) Space complexity : O(n) Using OrderedDict & Counter
+
+
+class OrderedCounter(Counter, OrderedDict):
+    'Counter that remembers the order elements are first encountered'
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
+
+    def __reduce__(self):
+        return self.__class__, (OrderedDict(self),)
 
 
 class Solution:
