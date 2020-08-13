4d41859148d92aa6d7fd3e9e38d3d7eebb1b4c80
PremPatel8 <PremPatel_8@outlook.com>
2020-08-12 21:51:59 -0500

    Alt solution commented

--- LeetCode/Valid Palindrome.py
+++ LeetCode/Valid Palindrome.py
@@ -49,3 +49,18 @@ myobj = Solution()
 inpt = "A man, a plan, a canal: Panama"
 # inpt = "race a car"
 print(myobj.isPalindrome(inpt))
+
+
+""" 
+def isPalindrome(self, s):
+    l, r = 0, len(s)-1
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l <r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l +=1; r -= 1
+    return True
+ """
