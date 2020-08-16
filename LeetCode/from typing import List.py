ec17c077fd21693f85e4af3f03f60a7ebbea57ed
PremPatel8 <PremPatel_8@outlook.com>
2020-08-16 03:45:40 -0500

    itertools approach simplified

--- LeetCode/from typing import List.py
+++ LeetCode/from typing import List.py
@@ -38,16 +38,28 @@ Runtime: 24 ms
 Memory Usage: 14.2 MB """
 
 # Solution techniques are
-# Time complexity : O() Space complexity : O() My solution using DP and string iteration to get digit freq counts
+# Time complexity : O() Space complexity : O() itertools approach simplified
 
 
 class Solution:
-    def countAndSay(self, n):
-        s = '1'
+    def countAndSay(self, n: 'int') -> 'str':
+        def directed_count_value(s):
+            temp, count = '', 1
+            for i in range(len(s) - 1):
+                if s[i] == s[i + 1]:
+                    count += 1
+                else:
+                    temp += str(count) + s[i]
+                    count = 1
+
+            temp += str(count) + s[-1]
+            return temp
+
+        result = '1'
         for _ in range(n - 1):
-            s = ''.join(str(len(list(group))) + digit
-                        for digit, group in itertools.groupby(s))
-        return s
+            result = directed_count_value(result)
+
+        return result
 
 
 myobj = Solution()
