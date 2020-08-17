a5c447a93fefa49f77e477c5b7236bb46c0df2d5
PremPatel8 <PremPatel_8@outlook.com>
2020-08-16 22:39:21 -0500

    two pointer & reversing the second half of the LL

---
+++ LeetCode/Palindrome Linked List.py
@@ -0,0 +1,64 @@
+from typing import List
+
+"""
+Problem Name: Palindrome Linked List
+
+Problem Section: Linked List
+
+Problem Statement:
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+Input: 1->2
+Output: false
+
+Example 2:
+Input: 1->2->2->1
+Output: true
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+Resources:
+
+"""
+""" 208 / 208 test cases passed.
+	Status: Accepted
+Runtime: 34 ms
+Memory Usage: 13.7 MB """
+
+# Solution techniques are
+# Time complexity : O(n) Space complexity : O(1) two pointer with reversing the second half of the linked list
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = slow = head
+        # find the mid node
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # reverse the second half
+        node = None
+        while slow:
+            nxt = slow.next
+            slow.next = node
+            node = slow
+            slow = nxt
+        # compare the first and second half nodes
+        while node:  # while node and head:
+            if node.val != head.val:
+                return False
+            node = node.next
+            head = head.next
+        return True
+
+
+myobj = Solution()
+
+print(myobj.mergeTwoLists(node))
