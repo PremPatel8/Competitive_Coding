029a28cfb2b481d18702f10a0fabed0d9d24cec1
PremPatel8 <PremPatel_8@outlook.com>
2020-08-24 17:18:14 -0500

    Inorder traversal solution

--- LeetCode/Validate Binary Search Tree.py
+++ LeetCode/Validate Binary Search Tree.py
@@ -39,41 +39,140 @@ Resources:
 """
 """ 75 / 75 test cases passed.
 	Status: Accepted
-Runtime: 44 ms
+Runtime: 48 ms
 Memory Usage: 16 MB """
 
 # Solution techniques are Recursion, Iteration using Stack and DFS, Inorder traversal
-# Time complexity : O(n) Space complexity : O(n) Iteration using Stack and DFS. Time - since we visit each node exactly once., Space - since we keep up to the entire tree.
+# Time complexity : O(n) Space complexity : O(n) Inorder traversal solution
+# Time complexity : O(N)in the worst case when the tree is BST or the "bad" element is a rightmost leaf.
+# Space complexity : O(N) to keep stack.
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
-            return True
-
-        stack = [(root, float('-inf'), float('inf')), ]
-        while stack:
-            root, lower, upper = stack.pop()
-            if not root:
-                continue
-            val = root.val
-            if val <= lower or val >= upper:
-                return False
-            stack.append((root.right, val, upper))
-            stack.append((root.left, lower, val))
-        return True
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
-myobj = Solution()
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
 
-print(myobj.maxDepth(node))
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def prettyPrintTree(node, prefix="", isLeft=True):
+    if not node:
+        print("Empty Tree")
+        return
+
+    if node.right:
+        prettyPrintTree(node.right, prefix +
+                        ("│   " if isLeft else "    "), False)
+
+    print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+    if node.left:
+        prettyPrintTree(node.left, prefix +
+                        ("    " if isLeft else "│   "), True)
+
+
+def isValidBST(root):
+    """
+    :type root: TreeNode
+    :rtype: bool
+    """
+    stack = []
+    inorder = float('-inf')
+
+    while stack or root:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        # If next element in inorder traversal
+        # is smaller than the previous one
+        # that's not BST.
+        if root.val <= inorder:
+            return False
+        inorder = root.val
+        root = root.right
+
+    return True
+
+
+def main():
+    import sys
+
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            node = stringToTreeNode(line)
+
+            print(isValidBST(node))
+
+            prettyPrintTree(node)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Input = [5, 1, 4, null, null, 3, 6], False
+# [2,1,3], True
