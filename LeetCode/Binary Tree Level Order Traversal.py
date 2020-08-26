e35b9328543c9a5da23187b3fd7262bdb841af2f
PremPatel8 <PremPatel_8@outlook.com>
2020-08-26 14:50:21 -0500

    BFS solution using Queue

---
+++ LeetCode/Binary Tree Level Order Traversal.py
@@ -0,0 +1,180 @@
+from typing import List
+from collections import deque
+
+"""
+Problem Name: Binary Tree Level Order Traversal
+
+Problem Section: Trees
+
+Problem Statement:
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+
+Resources:
+
+"""
+""" 34 / 34 test cases passed.
+	Status: Accepted
+Runtime: 28 ms
+Memory Usage: 14.1 MB """
+
+# Solution techniques are BFS
+# Time complexity : O(n) Space complexity : O(n) BFS solution using Queue
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def prettyPrintTree(node, prefix="", isLeft=True):
+    if not node:
+        print("Empty Tree")
+        return
+
+    if node.right:
+        prettyPrintTree(node.right, prefix +
+                        ("│   " if isLeft else "    "), False)
+
+    print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+    if node.left:
+        prettyPrintTree(node.left, prefix +
+                        ("    " if isLeft else "│   "), True)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+
+        if root is None:
+            return res
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(level)
+
+        return res
+
+
+def main():
+    import sys
+
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            node = stringToTreeNode(line)
+
+            myobj = Solution()
+            # inpt = [3,9,20,null,null,15,7]
+            print(myobj.levelOrder(node))
+
+            prettyPrintTree(node)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Input = [3,9,20,null,null,15,7],
+# Output = [ [3], [9,20], [15,7] ]
