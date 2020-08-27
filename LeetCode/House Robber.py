fedbd66ce8f40b104b0f1244ec8669b4d1237043
PremPatel8 <PremPatel_8@outlook.com>
2020-08-27 17:56:37 -0500

    All 4 DP solution techniques added

--- LeetCode/House Robber.py
+++ LeetCode/House Robber.py
@@ -34,7 +34,7 @@ Resources:
 Runtime: 24 ms
 Memory Usage: 13.8 MB """
 
-# Solution techniques are
+# Solution techniques are Plain Recursive (top-down), Recursive + memo (top-down), Iterative + memo (bottom-up), Iterative + N variables (bottom-up)
 # Time complexity : O(n) Space complexity : O(1) DP Iterative Bottom Up solution using 2 variables
 
 
@@ -57,3 +57,72 @@ class Solution:
 myobj = Solution()
 inpt = [2, 7, 9, 3, 1]
 print(myobj.rob(inpt))
+
+
+# Step 2. Recursive (top-down)
+""" Converting the recurrent relation from Step 1 shound't be very hard.
+
+public int rob(int[] nums) {
+    return rob(nums, nums.length - 1);
+}
+private int rob(int[] nums, int i) {
+    if (i < 0) {
+        return 0;
+    }
+    return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+}
+
+This algorithm will process the same i multiple times and it needs improvement. Time complexity: [to fill] """
+
+# Step 3. Recursive + memo (top-down).
+
+""" int[] memo;
+public int rob(int[] nums) {
+    memo = new int[nums.length + 1];
+    Arrays.fill(memo, -1);
+    return rob(nums, nums.length - 1);
+}
+
+private int rob(int[] nums, int i) {
+    if (i < 0) {
+        return 0;
+    }
+    if (memo[i] >= 0) {
+        return memo[i];
+    }
+    int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    memo[i] = result;
+    return result;
+}
+
+Much better, this should run in O(n) time. Space complexity is O(n) as well, because of the recursion stack, let's try to get rid of it. """
+
+# Step 4. Iterative + memo (bottom-up)
+
+""" public int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    int[] memo = new int[nums.length + 1];
+    memo[0] = 0;
+    memo[1] = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        int val = nums[i];
+        memo[i+1] = Math.max(memo[i], memo[i-1] + val);
+    }
+    return memo[nums.length];
+} """
+
+# Step 5. Iterative + 2 variables (bottom-up)
+""" We can notice that in the previous step we use only memo[i] and memo[i-1], so going just 2 steps back. We can hold them in 2 variables instead. This optimization is met in Fibonacci sequence creation and some other problems [to paste links].
+
+/* the order is: prev2, prev1, num  */
+public int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    int prev1 = 0;
+    int prev2 = 0;
+    for (int num : nums) {
+        int tmp = prev1;
+        prev1 = Math.max(prev2 + num, prev1);
+        prev2 = tmp;
+    }
+    return prev1;
+} """
