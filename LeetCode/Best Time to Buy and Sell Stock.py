cc2611a9d3e283b1627eac91a8b98e66e98ac539
PremPatel8 <PremPatel_8@outlook.com>
2020-08-27 15:24:54 -0500

    Iterative Greedy solution

---
+++ LeetCode/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,59 @@
+from typing import List
+
+"""
+Problem Name: Best Time to Buy and Sell Stock
+
+Problem Section: Dynamic Programming
+
+Problem Statement:
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+Example 2:
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+Resources:
+
+"""
+""" 200 / 200 test cases passed.
+	Status: Accepted
+Runtime: 64 ms
+Memory Usage: 15.2 MB """
+
+# Solution techniques are
+# Time complexity : O(n) Space complexity : O(1) Iterative Greedy solution
+""" 
+Complexity Analysis
+Time complexity : O(n) Only a single pass is needed.
+Space complexity : O(1) Only two variables are used.
+"""
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price-min_price > max_profit:
+                max_profit = price-min_price
+
+        return max_profit
+
+
+myobj = Solution()
+inpt = [7, 1, 5, 3, 6, 4]
+print(myobj.maxProfit(inpt))
