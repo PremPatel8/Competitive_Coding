bfbf8000ce8c4fe433181fdc274410cde1473201
PremPatel8 <PremPatel_8@outlook.com>
2020-08-27 17:51:56 -0500

    DP Iterative Bottom Up solution using 2 variables

---
+++ LeetCode/House Robber.py
@@ -0,0 +1,59 @@
+from typing import List
+
+"""
+Problem Name: House Robber
+
+Problem Section: Dynamic Programming
+
+Problem Statement:
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+Constraints:
+0 <= nums.length <= 100
+0 <= nums[i] <= 400
+
+Resources:
+
+"""
+""" 69 / 69 test cases passed.
+	Status: Accepted
+Runtime: 24 ms
+Memory Usage: 13.8 MB """
+
+# Solution techniques are
+# Time complexity : O(n) Space complexity : O(1) DP Iterative Bottom Up solution using 2 variables
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        prev1 = 0
+        prev2 = 0
+
+        for num in nums:
+            tmp = prev1
+            prev1 = max(prev2 + num, prev1)
+            prev2 = tmp
+
+        return prev1
+
+
+myobj = Solution()
+inpt = [2, 7, 9, 3, 1]
+print(myobj.rob(inpt))
