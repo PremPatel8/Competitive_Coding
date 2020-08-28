722e51ab9a78efa7f36add14fe8abf2fc196ee6f
PremPatel8 <PremPatel_8@outlook.com>
2020-08-27 22:44:01 -0500

    Iterative solution using Dict

---
+++ LeetCode/Roman to Integer.py
@@ -0,0 +1,74 @@
+from typing import List
+
+"""
+Problem Name: Roman to Integer
+
+Problem Section: Math
+
+Problem Statement:
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. 
+The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Examples:
+Input: "III"
+Output: 3
+
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Resources:
+
+"""
+""" 3999 / 3999 test cases passed.
+	Status: Accepted
+Runtime: 40 ms
+Memory Usage: 13.8 MB """
+
+# Solution techniques are
+# Time complexity : O(n) Space complexity : O(1) Iterative solution using Dict
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'M': 1000, 'D': 500, 'C': 100,
+                 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        total = 0
+
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+
+        return total + roman[s[-1]]
+
+
+myobj = Solution()
+inpt = "MCMXCIV"
+print(myobj.romanToInt(inpt))
