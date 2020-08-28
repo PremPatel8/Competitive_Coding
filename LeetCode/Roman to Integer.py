e8ca7482e8612ade9245e1e49259404ccddbbaeb
PremPatel8 <PremPatel_8@outlook.com>
2020-08-27 22:46:01 -0500

    Reverse Iterative solution using Dict

--- LeetCode/Roman to Integer.py
+++ LeetCode/Roman to Integer.py
@@ -48,25 +48,27 @@ Resources:
 """ 3999 / 3999 test cases passed.
 	Status: Accepted
 Runtime: 40 ms
-Memory Usage: 13.8 MB """
+Memory Usage: 13.7 MB """
 
 # Solution techniques are
-# Time complexity : O(n) Space complexity : O(1) Iterative solution using Dict
+# Time complexity : O(n) Space complexity : O(1) Reverse Iterative solution using Dict
 
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {'M': 1000, 'D': 500, 'C': 100,
-                 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        _dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        prev = 0
         total = 0
 
-        for i in range(0, len(s) - 1):
-            if roman[s[i]] < roman[s[i+1]]:
-                total -= roman[s[i]]
+        for i in s[::-1]:
+            curr = _dict[i]
+            if prev > curr:
+                total -= curr
             else:
-                total += roman[s[i]]
-
-        return total + roman[s[-1]]
+                total += curr
+            prev = curr
+        return total
 
 
 myobj = Solution()
