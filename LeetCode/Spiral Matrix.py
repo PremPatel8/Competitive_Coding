139d3255ad257f1972a25b4e7bcdd14c93e5ee5a
PremPatel8 <PremPatel_8@outlook.com>
2020-09-10 00:54:15 -0500

    Layer-by-Layer solution

--- LeetCode/Spiral Matrix.py
+++ LeetCode/Spiral Matrix.py
@@ -36,12 +36,12 @@ Resources:
 
 """ 22 / 22 test cases passed.
 	Status: Accepted
-Runtime: 32 ms
-Memory Usage: 13.9 MB """
+Runtime: 28 ms
+Memory Usage: 13.8 MB """
 
 # Solution techniques are Simulation, Layer-by-Layer
 
-# Time complexity : O(n) Space complexity : O(n) Simulation solution using a seen matrix and two r and c arrays for performing a clockwise turn using di = (di + 1) % 4 to loop between 0 to 4
+# Time complexity : O(n) Space complexity : O(1) Layer-by-Layer solution
 
 
 class Solution:
@@ -49,24 +49,27 @@ class Solution:
         if not matrix:
             return []
 
-        R, C = len(matrix), len(matrix[0])
-        seen = [[False] * C for _ in matrix]
         ans = []
-        dr = [0, 1, 0, -1]
-        dc = [1, 0, -1, 0]
-        r = c = di = 0
+        r1, r2 = 0, len(matrix) - 1
+        c1, c2 = 0, len(matrix[0]) - 1
 
-        for _ in range(R * C):
-            ans.append(matrix[r][c])
-            seen[r][c] = True
+        while r1 <= r2 and c1 <= c2:
+            for c in range(c1, c2 + 1):
+                ans.append(matrix[r1][c])
 
-            cr, cc = r + dr[di], c + dc[di]
+            for r in range(r1 + 1, r2 + 1):
+                ans.append(matrix[r][c2])
 
-            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
-                r, c = cr, cc
-            else:
-                di = (di + 1) % 4
-                r, c = r + dr[di], c + dc[di]
+            if r1 < r2 and c1 < c2:
+                for c in range(c2 - 1, c1, -1):
+                    ans.append(matrix[r2][c])
+                for r in range(r2, r1, -1):
+                    ans.append(matrix[r][c1])
+
+            r1 += 1
+            r2 -= 1
+            c1 += 1
+            c2 -= 1
 
         return ans
 
