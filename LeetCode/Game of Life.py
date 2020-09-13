c6d9ea51cc319421ce64b7919bc5229b71f55325
PremPatel8 <PremPatel_8@outlook.com>
2020-09-13 13:47:31 -0500

    2 solutions

---
+++ LeetCode/Game of Life.py
@@ -0,0 +1,158 @@
+from typing import List
+
+"""
+Problem Name: Game of Life
+
+Problem Section: Array and Strings
+
+Problem Statement:
+According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population..
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+Write a function to compute the next state (after one update) of the board given its current state. The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
+
+Example:
+
+Input:
+[
+  [0,1,0],
+  [0,0,1],
+  [1,1,1],
+  [0,0,0]
+]
+Output:
+[
+  [0,0,0],
+  [1,0,1],
+  [0,1,1],
+  [0,1,0]
+]
+
+Follow up:
+Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
+In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
+
+
+Resources:
+
+"""
+
+""" 23 / 23 test cases passed.
+	Status: Accepted
+Runtime: 56 ms
+Memory Usage: 13.7 MB """
+
+# Solution techniques are Make a copy of original board for reference, Update the orginal board and record the previous cell value using -1(previously live now dead) or 2(previously dead now live) for
+
+# Time complexity : O(M X N) Space complexity : O(M X N) Make a copy of original board for reference
+#  where M is the number of rows and N is the number of columns of the Board.
+
+
+# class Solution:
+#     def gameOfLife(self, board: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify board in-place instead.
+#         """
+
+#         # Neighbors array to find 8 neighboring cells for a given cell
+#         neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1),
+#                      (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+#         rows = len(board)
+#         cols = len(board[0])
+
+#         # Create a copy of the original board
+#         copy_board = [[board[row][col]
+#                        for col in range(cols)] for row in range(rows)]
+
+#         # Iterate through board cell by cell.
+#         for row in range(rows):
+#             for col in range(cols):
+
+#                 # For each cell count the number of live neighbors.
+#                 live_neighbors = 0
+#                 for neighbor in neighbors:
+
+#                     r = (row + neighbor[0])
+#                     c = (col + neighbor[1])
+
+#                     # Check the validity of the neighboring cell and if it was originally a live cell.
+#                     # The evaluation is done against the copy, since that is never updated.
+#                     if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+#                         live_neighbors += 1
+
+#                 # Rule 1 or Rule 3
+#                 if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+#                     board[row][col] = 0
+#                 # Rule 4
+#                 if copy_board[row][col] == 0 and live_neighbors == 3:
+#                     board[row][col] = 1
+
+#         return board
+
+
+""" 23 / 23 test cases passed.
+	Status: Accepted
+Runtime: 32 ms
+Memory Usage: 14 MB """
+
+# Time complexity : O(M X N) Space complexity : O(1) Update the original board and record previous state as well.
+#  where M is the number of rows and N is the number of columns of the Board.
+
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1),
+                     (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        for row in range(rows):
+            for col in range(cols):
+
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and abs(board[r][c]) == 1:
+                        live_neighbors += 1
+
+                # Rule 1 or Rule 3
+                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    # -1 signifies the cell is now dead but originally was live.
+                    board[row][col] = -1
+                # Rule 4
+                if board[row][col] == 0 and live_neighbors == 3:
+                    # 2 signifies the cell is now live but was originally dead.
+                    board[row][col] = 2
+
+        # Get the final representation for the newly updated board.
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0
+
+        return board
+
+
+myobj = Solution()
+inpt = [
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0]
+]
+print(myobj.gameOfLife(inpt))
