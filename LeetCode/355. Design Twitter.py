248697ede11d1fa0886b36421d088c96543156d6
PremPatel8 <PremPatel_8@outlook.com>
2020-10-13 18:47:53 -0500

    leetcode sol

---
+++ LeetCode/355. Design Twitter.py
@@ -0,0 +1,91 @@
+from typing import List
+import itertools
+import collections
+import heapq
+
+"""
+Problem Name: 355. Design Twitter
+
+Problem URL: https://leetcode.com/problems/design-twitter/
+
+Problem Section: Dict, Heap
+
+Problem Statement:
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+postTweet(userId, tweetId): Compose a new tweet.
+getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+follow(followerId, followeeId): Follower follows a followee.
+unfollow(followerId, followeeId): Follower unfollows a followee.
+
+Example:
+Twitter twitter = new Twitter();
+
+// User 1 posts a new tweet (id = 5).
+twitter.postTweet(1, 5);
+
+// User 1's news feed should return a list with 1 tweet id -> [5].
+twitter.getNewsFeed(1);
+
+// User 1 follows user 2.
+twitter.follow(1, 2);
+
+// User 2 posts a new tweet (id = 6).
+twitter.postTweet(2, 6);
+
+// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.getNewsFeed(1);
+
+// User 1 unfollows user 2.
+twitter.unfollow(1, 2);
+
+// User 1's news feed should return a list with 1 tweet id -> [5],
+// since user 1 is no longer following user 2.
+twitter.getNewsFeed(1);
+
+
+Resources:
+https://leetcode.com/problems/design-twitter/discuss/82863/Python-solution
+
+runtime: 
+23 / 23 test cases passed.
+	Status: Accepted
+Runtime: 84 ms
+Memory Usage: 19.6 MB
+
+"""
+
+# Solution techniques are make a userid to tweet deque dict, append new tweets to the left of deque with a timer / counter variable to mark insertion order, to retrieve top 10 tweets
+# do a union of all followee ids and gather tweets from all and then merge using heap and get top 10
+
+# Time complexity : O() Space complexity : O()
+
+
+class Twitter:
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId):
+        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))
+        return [t for _, t in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId, followeeId):
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        self.followees[followerId].discard(followeeId)
+        
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
