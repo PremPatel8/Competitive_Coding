39862bc8803def97411d5f74ed55f7f5e5cd6aa3
PremPatel8 <PremPatel_8@outlook.com>
2020-10-13 21:11:32 -0500

    incomplete

---
+++ LeetCode/829. Consecutive Numbers Sum.py
@@ -0,0 +1,52 @@
+from typing import List
+
+"""
+Problem Name: 829. Consecutive Numbers Sum
+
+Problem URL: https://leetcode.com/problems/consecutive-numbers-sum/
+
+Problem Section: Math
+
+Problem Statement:
+Given a positive integer N, how many ways can we write it as a sum of consecutive positive integers?
+
+Example 1:
+
+Input: 5
+Output: 2
+Explanation: 5 = 5 = 2 + 3
+
+Example 2:
+
+Input: 9
+Output: 3
+Explanation: 9 = 9 = 4 + 5 = 2 + 3 + 4
+
+Example 3:
+
+Input: 15
+Output: 4
+Explanation: 15 = 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+
+Note: 1 <= N <= 10 ^ 9.
+
+
+Resources:
+
+runtime: 
+
+"""
+
+# Solution techniques are
+
+# Time complexity : O() Space complexity : O()
+
+
+class Solution:
+    def consecutiveNumbersSum(self, N: int) -> int:
+        pass
+
+
+myobj = Solution()
+inpt = 5
+print(myobj.consecutiveNumbersSum(inpt))
