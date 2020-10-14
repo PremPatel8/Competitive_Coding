e65aaba045ae39dc763f62d34fd14c391cefb7ab
PremPatel8 <PremPatel_8@outlook.com>
2020-10-13 19:29:27 -0500

    Two pointer solution

--- LeetCode/532. K-diff Pairs in an Array.py
+++ LeetCode/532. K-diff Pairs in an Array.py
@@ -31,12 +31,14 @@ Although we have two 1s in the input, we should only return the number of unique
 
 Resources:
 https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/100135/JavaPython-Easy-Understood-Solution
+https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/150243/Easy-to-understand-2-pointer-sliding-window-approach-in-python-O(1)-space-O(nlogn)-time
+https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/100104/Two-pointer-Approach
 
 runtime: 
 59 / 59 test cases passed.
 	Status: Accepted
-Runtime: 72 ms
-Memory Usage: 15.6 MB
+Runtime: 84 ms
+Memory Usage: 15.1 MB
 """
 
 # Solution techniques are
@@ -45,12 +47,43 @@ Memory Usage: 15.6 MB
 
 
 class Solution:
+    # def findPairs(self, nums: List[int], k: int) -> int:
+    #     res = 0
+    #     num_freq = collections.Counter(nums)
+    #     for no in num_freq:
+    #         if (k > 0 and no + k in num_freq) or (k == 0 and num_freq[no] > 1):
+    #             res += 1
+    #     return res
+
+    # Two pointer solution
     def findPairs(self, nums: List[int], k: int) -> int:
+        size = len(nums)
         res = 0
-        num_freq = collections.Counter(nums)
-        for no in num_freq:
-            if (k > 0 and no + k in num_freq) or (k == 0 and num_freq[no] > 1):
+        slow = 0
+        fast = 1
+
+        nums.sort()
+
+        while fast < size:
+            if nums[fast] - nums[slow] < k:  # case 1, diff is less than k
+                fast += 1
+            elif nums[fast] - nums[slow] > k:  # case 2, diff is greater than k
+                slow += 1
+            else:  # case 3, diff is equal to k so increment the res!
                 res += 1
+                fast += 1
+                slow += 1
+
+                # Now ignore any duplicates, both slow and fast could be pointing to duplicates
+                while slow < size-1 and nums[slow] == nums[slow-1]:
+                    slow += 1
+
+                while fast < size-1 and nums[fast] == nums[fast-1]:
+                    fast += 1
+
+            if fast <= slow:  # fast should be atleast one more than slow
+                fast = slow + (slow - fast) + 1
+
         return res
 
 
