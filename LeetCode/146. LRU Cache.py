7dcdc6bc08e5aa25259940ee56a5b0365f3a2ecb
Prem Patel <prempatel_8@outlook.com>
2020-11-04 03:23:52 -0600

    OrderedDict Sol

--- LeetCode/146. LRU Cache.py
+++ LeetCode/146. LRU Cache.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import OrderedDict
 
 """
 Problem Name: 146. LRU Cache
@@ -57,73 +58,37 @@ Resources:
 runtime: 
 18 / 18 test cases passed.
 	Status: Accepted
-Runtime: 192 ms
+Runtime: 148 ms
 Memory Usage: 23.4 MB
 """
 
-# Solution techniques are Dict with Key as key and value as Doubly Linked List node & Doubly Linked List to maintain LRU order of Nodes with LRU node at tail and most recently used node at head
+# Solution techniques are OrderedDict with move_to_end and popitem FIFO order
 
 # Time complexity : Get & Put O(1) Space complexity : O(n) number of key, val pairs
-
-
-class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.prev = None
 
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.keyDict = {}
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def addNode(self, node):
-        head_next = self.head.next
-        node.next = head_next
-        head_next.prev = node
-        self.head.next = node
-        node.prev = self.head
-
-    def removeNode(self, node):
-        next_node = node.next
-        prev_node = node.prev
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def moveNodeToFront(self, node):
-        self.removeNode(node)
-        self.addNode(node)
+        self.keyDict = OrderedDict()
 
     def get(self, key: int) -> int:
         res = -1
 
         if key in self.keyDict:
-            node = self.keyDict[key]
-            res = node.val
-            self.moveNodeToFront(node)
+            self.keyDict.move_to_end(key)
+            res = self.keyDict[key]
 
         return res
 
     def put(self, key: int, value: int) -> None:
         if key in self.keyDict:
-            node = self.keyDict[key]
-            node.val = value
-            self.moveNodeToFront(node)
+            self.keyDict.move_to_end(key)
         else:
-            newNode = Node(key, value)
-
             if len(self.keyDict) >= self.capacity:
-                del self.keyDict[self.tail.prev.key]
-                self.removeNode(self.tail.prev)
+                self.keyDict.popitem(last=False)
 
-            self.keyDict[key] = newNode
-            self.addNode(newNode)
+        self.keyDict[key] = value
 
 
 # Your LRUCache object will be instantiated and called as such:
