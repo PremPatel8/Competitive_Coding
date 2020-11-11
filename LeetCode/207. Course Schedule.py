995aa8e14aa250a3d30269182457b4f401cb1a87
Prem Patel <prempatel_8@outlook.com>
2020-11-11 02:24:48 -0600

    DFS with vertex state array, DFS with stack + set (Top Sort), BFS kahn's algorithm with inDegree array and queue

--- LeetCode/207. Course Schedule.py
+++ LeetCode/207. Course Schedule.py
@@ -48,46 +48,184 @@ Memory Usage: 15.1 MB
 # Time complexity : O(V+E) Space complexity : O(V+E)
 
 
+# https://leetcode.com/problems/course-schedule/discuss/441722/Python-99-time-and-100-space.-Collection-of-solutions-with-explanation
+
+# DFS with vertex state array
+# DFS with stack + set (Top Sort)
+# BFS kahn's algorithm with inDegree array and queue
+
+
 class Solution:
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        graph = [[] for _ in range(numCourses)]
-        visited = [0 for _ in range(numCourses)]
-        # create graph
-        for pair in prerequisites:
-            x, y = pair
-            graph[x].append(y)
-        # visit each node
-        for i in range(numCourses):
-            if not self.dfs(graph, visited, i):
+
+    # Solution 1: DFS with an array storing 3 different states of a vertex
+
+    def buildAdjacencyList(self, n, edgesList):
+        adjList = [[] for _ in range(n)]
+        # c2 (course 2) is a prerequisite of c1 (course 1)
+        # i.e c2c1 is a directed edge in the graph
+        for c1, c2 in edgesList:
+            adjList[c2].append(c1)
+        return adjList
+
+    """ 46 / 46 test cases passed.
+            Status: Accepted
+        Runtime: 88 ms
+        Memory Usage: 16.7 MB """
+
+    """ def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # build Adjacency list from Edges list
+        adjList = self.buildAdjacencyList(numCourses, prerequisites)
+
+        # Each vertex can have 3 different states:
+        # state 0   : vertex is not visited. It's a default state.
+        # state -1  : vertex is being processed. Either all of its descendants
+        #             are not processed or it's still in the function call stack.
+        # state 1   : vertex and all its descendants are processed.
+        state = [0] * numCourses
+
+        def hasCycle(v):
+            if state[v] == 1:
+                # This vertex is processed so we pass.
                 return False
-        return True
-    
-    def dfs(self, graph, visited, i):
-        # if ith node is marked as being visited, then a cycle is found
-        if visited[i] == -1:
+            if state[v] == -1:
+                # This vertex is being processed and it means we have a cycle.
+                return True
+
+            # Set state to -1
+            state[v] = -1
+
+            for i in adjList[v]:
+                if hasCycle(i):
+                    return True
+
+            state[v] = 1
             return False
-        # if it is done visted, then do not visit again
-        if visited[i] == 1:
-            return True
-        # mark as being visited
-        visited[i] = -1
-        # visit all the neighbours
-        for j in graph[i]:
-            if not self.dfs(graph, visited, j):
+
+        # we traverse each vertex using DFS, if we find a cycle, stop and return
+        for v in range(numCourses):
+            if hasCycle(v):
                 return False
-        # after visit all the neighbours, mark it as done visited
-        visited[i] = 1
-        return True
+
+        return True """
+
+# Solution 2: DFS with a stack storing all decendants being processed
+# Same idea as Solution 1, this time we use a stack to store all vertices being processed. While visiting a descendant of a vertex, if we found it in the stack it means a cycle appears.
+# This technique is also used to find a Topological order from the graph.
+
+    """ 46 / 46 test cases passed.
+        Status: Accepted
+    Runtime: 100 ms
+    Memory Usage: 17.3 MB """
+
+    """ def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # build Adjacency list from Edges list
+        adjList = self.buildAdjacencyList(numCourses, prerequisites)
+        visited = set()
+
+        def hasCycle(v, stack):
+            if v in visited:
+                if v in stack:
+                    # This vertex is being processed and it means we have a cycle.
+                    return True
+                # This vertex is processed so we pass
+                return False
+
+            # mark this vertex as visited
+            visited.add(v)
+            # add it to the current stack
+            stack.append(v)
+
+            for i in adjList[v]:
+                if hasCycle(i, stack):
+                    return True
+
+            # once processed, we pop it out of the stack
+            stack.pop()
+            return False
+
+        # we traverse each vertex using DFS, if we find a cycle, stop and return
+        for v in range(numCourses):
+            if hasCycle(v, []):
+                return False
+
+        return True """
+
+
+# Solution 3: BFS with Kahn's algorithm for Topological Sorting
+# This solution is usually seen in problems where we need to answer two questions:
+# Is it possible to have a topological order?
+# if yes then print out one of all the orders.
+
+    """ 46 / 46 test cases passed.
+        Status: Accepted
+    Runtime: 84 ms
+    Memory Usage: 15.2 MB """
+
+    def topoBFS(self, numNodes, edgesList):
+        # Note: for consistency with other solutions above, we keep building
+        # an adjacency list here. We can also merge this step with the next step.
+        adjList = self.buildAdjacencyList(numNodes, edgesList)
+
+        # 1. A list stores No. of incoming edges of each vertex
+        inDegrees = [0] * numNodes
+        for v1, v2 in edgesList:
+            # v2v1 form a directed edge
+            inDegrees[v1] += 1
+
+        # 2. a queue of all vertices with no incoming edge
+        # at least one such node must exist in a non-empty acyclic graph
+        # vertices in this queue have the same order as the eventual topological
+        # sort
+        queue = []
+        for v in range(numNodes):
+            if inDegrees[v] == 0:
+                queue.append(v)
+
+        # initialize count of visited vertices
+        count = 0
+        # an empty list that will contain the final topological order
+        topoOrder = []
+
+        while queue:
+            # a. pop a vertex from front of queue
+            # depending on the order that vertices are removed from queue,
+            # a different solution is created
+            v = queue.pop(0)
+            # b. append it to topoOrder
+            topoOrder.append(v)
+
+            # increase count by 1
+            count += 1
+
+            # for each descendant of current vertex, reduce its in-degree by 1
+            for des in adjList[v]:
+                inDegrees[des] -= 1
+                # if in-degree becomes 0, add it to queue
+                if inDegrees[des] == 0:
+                    queue.append(des)
+
+        if count != numNodes:
+            return None  # graph has at least one cycle
+        else:
+            return topoOrder
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        return True if self.topoBFS(numCourses, prerequisites) else False
 
 
 myobj = Solution()
-numCourses = 2
+# numCourses = 2
 # prerequisites = [[1, 0], [0, 1]]
-prerequisites = [[1, 0]]
+# prerequisites = [[1, 0]]
+
+numCourses = 8
+prerequisites = [[0, 6], [1, 6], [1, 4], [1, 2],
+                 [3, 0], [3, 4], [5, 1], [7, 0], [7, 1]]
+
+# Expected Adjacency List: [0:[3, 7], 1:[5, 7], 2:[1], 3:[], 4:[1, 3], 5:[], 6:[0, 1], 7:[]]
+#    0        1     2    3    4     5      6     7
+# [[3, 7], [5, 7], [1], [], [1, 3], [], [0, 1], []]
+# Explanation: Course 0 is a prerequisite for courses 3 & 7
+
 
 print(myobj.canFinish(numCourses, prerequisites))
