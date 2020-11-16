ebb494e6e8834ab8bcf14acc0b39568ad246078d
Prem Patel <prempatel_8@outlook.com>
2020-11-16 13:01:56 -0600

    Ccomments added for clarification

--- LeetCode/Find_All_Anagrams_in_a_String.py
+++ LeetCode/Find_All_Anagrams_in_a_String.py
@@ -29,6 +29,7 @@ The substring with start index = 6 is "bac", which is an anagram of "abc".
 
 
 Resources:
+https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/636988/Sliding-Window-or-HashTable-or-Java-Explained-with-Diagram-Beats-99
 
 runtime:
 
@@ -36,7 +37,23 @@ runtime:
 
 # Solution techniques are
 
-# Time complexity : O() Space complexity : O()
+# Time complexity : O(S) Space complexity : O(1)
+""" Time Complexity - O(S)
+Each array element element would be traversed at most twice . First time by windowEnd to decrement the value, then by windowStart to increment . Best case would be when none of the array elements are part of anagram.
+eg - P = 'abc' S = 'xyzr' . Here, both windowEnd and windowStart would be incremented at same time.
+
+Space complexity - O(1)
+Since we are using constant extra space (map) of size 26 / Dict used by me but at max there will be 26 keys for each alphabet in the dict"""
+
+""" Question - How are we checking for valid anagrams without actually traversing over map
+The idea is that window Size (windowEnd-windowStart) would only we equal to length of P when all the characters in window are valid anagram. (Point 3 above).
+Example - P = aab S = aaabca. Here, map = [ 'a' = 2, 'b' = 1].
+
+After traversing first 2 a's aa in S, map = ['a' = 0, 'b' = 1], window = aa.
+Now when we traverse 3rd a (aaa) in S, map count for 'a' is 0. So we know window is no longer valid anagram of P, and we increment start, now window has a only.
+See how we are reducing window size here when we dont find right anagram character In this example, window size would be only equal to 3, when it has characters a,a and b.
+Feel free to ask questions in comments. Do upvote if you understood the solution.
+ """
 
 
 class Solution:
