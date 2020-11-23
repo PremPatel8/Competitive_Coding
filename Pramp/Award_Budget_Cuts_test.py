aa058e89b378210fd40aff584bf6bbbcf5a18ded
Prem Patel <prempatel_8@outlook.com>
2020-11-23 17:26:31 -0600

    Working Pramp solution with unit tests

--- Pramp/Award_Budget_Cuts_test.py
+++ Pramp/Award_Budget_Cuts_test.py
@@ -23,30 +23,88 @@ space: O(1)
             l, r = ((m, r), (l, m))[total > b] """
 
 
+""" def find_grants_cap(grantsArray, newBudget):
+    grantsArray.sort(reverse=True)
+    grantsArrSum = sum(grantsArray)
+    grantsArrayLen = len(grantsArray)
+    grantsAffectedSum = 0
+
+    surplus = grantsArrSum-newBudget
+
+    if surplus <= 0:
+        return grantsArray[0]
+
+    grantsAffectedSum += grantsArray[0]
+
+    for i, grant in enumerate(grantsArray[1:], start=1):
+        newGrantsSum = grantsAffectedSum - i*grant
+
+        updatedSumDelta = grantsArrSum-newGrantsSum
+
+        if updatedSumDelta - newBudget <= 0:
+            diff = newBudget-updatedSumDelta
+
+            return grant + abs(diff)
+
+        grantsAffectedSum += grant """
+
+
 def find_grants_cap(grantsArray, newBudget):
-    noGrants = len(grantsArray)
-    avgGrant = newBudget / noGrants
+    n = len(grantsArray)
 
-    noAffected = 0
-    sumUnaffected = 0
+    grantsArray.sort(reverse=True)
 
-    for grant in grantsArray:
-        if grant <= avgGrant:
-            sumUnaffected += grant
-        elif grant > avgGrant:
-            noAffected += 1
+    grantsArray.append(0)
 
-    newCap = (newBudget-sumUnaffected) / noAffected
+    surplus = sum(grantsArray) - newBudget
 
-    return newCap
+    if (surplus <= 0):
+        return grantsArray[0]
+
+    i = 0
+    for j in range(n):
+        i = j
+        surplus -= (j+1) * (grantsArray[j] - grantsArray[j+1])
+
+        if (surplus <= 0):
+            break
+
+    return grantsArray[i+1] + (-surplus / float(i+1))
 
 
-def test_find_grants_cap():
-    # Pass
+# res = find_grants_cap(
+#     [210, 200, 150, 193, 130, 110, 209, 342, 117], 1530)
+# print(res)
+# print(res == 211)
+
+
+""" 
+assert find_grants_cap([2, 4, 6], 3) == 1
+assert find_grants_cap([21, 100, 50, 120, 130, 110], 140) == 23.8
+assert find_grants_cap([210, 200, 150, 193, 130, 110, 209, 342, 117], 1530) == 211
+"""
+
+
+def test_find_grants_cap_1():
     assert find_grants_cap([2, 100, 50, 120, 1000], 190) == 47
-    assert find_grants_cap([2, 4], 3) == 1.5
+
+
+def test_find_grants_cap_2():
     assert find_grants_cap([2, 4, 6], 3) == 1
+
+
+def test_find_grants_cap_3():
     assert find_grants_cap([21, 100, 50, 120, 130, 110], 140) == 23.8
-    # Fail
+
+
+def test_find_grants_cap_4():
     assert find_grants_cap(
         [210, 200, 150, 193, 130, 110, 209, 342, 117], 1530) == 211
+
+
+def test_find_grants_cap_5():
+    assert find_grants_cap([2, 4], 3) == 1.5
+
+
+def test_find_grants_cap_6():
+    assert find_grants_cap([2, 100, 50, 120, 167], 400) == 128
