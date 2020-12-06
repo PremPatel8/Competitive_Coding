9eaa5eac24c9a638ae46a7b0bebecfb292a94e8d
Prem Patel <prempatel_8@outlook.com>
2020-12-05 20:29:25 -0600

    Python sort by multiple keys using tuples and negating values to sort descending order

--- Pramp/Word Count Engine.py
+++ Pramp/Word Count Engine.py
@@ -3,6 +3,11 @@
 2 - dict - count the freq of all words, word as key and freq as value, add start pos
 3 - des order of freq, or start pos
 
+References:
+https://www.programiz.com/python-programming/methods/built-in/sorted
+https://docs.python.org/3/howto/sorting.html
+https://stackoverflow.com/questions/4233476/sort-a-list-by-multiple-attributes
+https://stackoverflow.com/questions/1143671/how-to-sort-objects-by-multiple-keys-in-python
 """
 
 from collections import defaultdict
@@ -32,17 +37,18 @@ def word_count_engine(document):
 
     # print("freqDict - ", freqDict)
 
-    # Python 3.X saves the insertion order of dict, so we only have to sort by freq
-    # For Python 2.X we have to sort twice, once by the word index in ascending order and then by the freq in descending order
-    index_sorted_words = sorted(
-        freqDict.keys(), key=lambda wrd: freqDict[wrd][1])
+    # Python 3.X saves the insertion order of dict, so we only have to sort by freq in descending order
+    # For Python 2.X we have to sort twice, once by the word index in ascending order and then by the freq in descending order (sort from least significant key to most significant key)
+    # Alternatively we can use tuples with key and use negative values to sort one key by descending and other by ascending
+    # Two tuples can be compared by comparing their elements starting from first. If there is a tie (elements are equal), the second element is compared, and so on.
 
-    freq_sorted_words = sorted(
-        index_sorted_words, key=lambda wrd: freqDict[wrd][0], reverse=True)
+    # we first compare the freq_count in descending order, if freq is same we compare by first_index in default ascending order
+    sorted_words = sorted(
+        freqDict.keys(), key=lambda wrd: (-freqDict[wrd][0], freqDict[wrd][1]))
 
     # print("sorted_words - ", sorted_words)
 
-    for word in freq_sorted_words:
+    for word in sorted_words:
         wrdFreq = freqDict[word][0]
         output.append([word, str(wrdFreq)])
 
