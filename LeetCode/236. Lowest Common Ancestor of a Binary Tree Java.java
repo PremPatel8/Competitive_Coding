9cb809c5c97641bb5ba7561ecc088d6f69ddae73
Prem Patel <prempatel_8@outlook.com>
2020-12-12 15:15:35 -0600

    Iterative using parent pointers

--- LeetCode/236. Lowest Common Ancestor of a Binary Tree Java.java
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree Java.java
@@ -1,27 +1,36 @@
-/* Problem Name: 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-Problem URL: 
+/* Problem Name: Lowest Common Ancestor of a Binary Tree
+
+Problem URL: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
 
 Problem Section: 
 
 Problem Statement:
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 Resources:
 
 runtime: 
 31 / 31 test cases passed.
 	Status: Accepted
-Runtime: 4 ms
-Memory Usage: 41.4 MB
+Runtime: 9 ms
+Memory Usage: 39.9 MB
 
  */
 
-// Solution techniques are Recursive Approach
+// Solution techniques are Iterative using parent pointers
 
 /* 
-Time Complexity: O(N), where N is the number of nodes in the binary tree. In the worst case we might be visiting all the nodes of the binary tree.
-Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed binary tree could be N.
+Time Complexity : O(N), where N is the number of nodes in the binary tree. In the worst case we might be visiting all the nodes of the binary tree.
+Space Complexity : O(N). In the worst case space utilized by the stack, the parent pointer dictionary and the ancestor set, would be N each, since the height of a skewed binary tree could be N.
 */
 
 // Definition for a binary tree node.
@@ -37,41 +46,47 @@ class TreeNode {
 
 class Solution {
 
-    private TreeNode ans;
-
-    public Solution() {
-        // Variable to store LCA node.
-        this.ans = null;
-    }
-
-    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-
-        // If reached the end of a branch, return false.
-        if (currentNode == null) {
-            return false;
-        }
-
-        // Left Recursion. If left recursion returns true, set left = 1 else 0
-        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
-
-        // Right Recursion
-        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
-
-        // If the current node is one of p or q
-        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
-
-        // If any two of the flags left, right or mid become True
-        if (mid + left + right >= 2) {
-            this.ans = currentNode;
-        }
-
-        // Return true if any one of the three bool values is True.
-        return (mid + left + right > 0);
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Traverse the tree
-        this.recurseTree(root, p, q);
-        return this.ans;
+
+        // Stack for tree traversal
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        // HashMap for parent pointers
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+
+        parent.put(root, null);
+        stack.push(root);
+
+        // Iterate until we find both the nodes p and q
+        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+
+            TreeNode node = stack.pop();
+
+            // While traversing the tree, keep saving the parent pointers.
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+
+        // Ancestors set() for node p.
+        Set<TreeNode> ancestors = new HashSet<>();
+
+        // Process all ancestors for node p using parent pointers.
+        while (p != null) {
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+
+        // The first ancestor of q which appears in
+        // p's ancestor set() is their lowest common ancestor.
+        while (!ancestors.contains(q))
+            q = parent.get(q);
+        return q;
     }
+
 }
