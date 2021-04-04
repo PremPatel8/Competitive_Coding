12f819aa19bd8ee8dfcbaf0ab02a3fea49cca35d
Prem Patel <prempatel_8@outlook.com>
2021-04-03 22:24:57 -0500

    alt sol

--- Binarysearch.com/Revolving Door.py
+++ Binarysearch.com/Revolving Door.py
@@ -44,7 +44,7 @@ class Solution:
 
         return ret """
 
-    def solve(self, requests):
+    """ def solve(self, requests):
         # events dict with key = time and value = [total no people of exiting at that time, total no of people entering at that time] - List/tuple
         events = defaultdict(lambda: [0, 0])
         for time, direction in requests:
@@ -74,13 +74,13 @@ class Solution:
             # first is the no of people going in the same direction as the current direction of the door for time - t
             # second is the no of people going in the other direction for time - t
 
-            """ 
-            ^ - XOR operator
-            logical operator that will return 1 when the bits are different and 0 elsewhere
+            
+            # ^ - XOR operator
+            # logical operator that will return 1 when the bits are different and 0 elsewhere
 
-            1 ^ 1 = 0
-            0 ^ 1 = 1
-            """
+            # 1 ^ 1 = 0
+            # 0 ^ 1 = 1
+           
             first = events[t][direction]
             second = events[t][direction ^ 1]
 
@@ -97,6 +97,41 @@ class Solution:
             # whenever we have people going in the oppsite direction of the current direction of door, we need to flip the door direction
             if second:
                 direction ^= 1
+
+        return ans """
+
+    def solve(self, requests):
+        if not requests:
+            return []
+
+        IN = 1
+        OUT = 0
+        ans = []
+        d = defaultdict(list)
+
+        for t, r in requests:
+            d[t] += [r]
+
+        requests = sorted(d.items(), key=lambda x: x[0])
+        door = IN
+        t = requests[0][0]
+
+        for time_of_req, reqs in requests:
+
+            if time_of_req > t:
+                t = time_of_req
+                door = IN
+
+            if door == IN:
+                reqs = reversed(sorted(reqs))
+            else:
+                reqs = sorted(reqs)
+
+            for r in reqs:
+                ans.append([t, r])
+                t += 1
+
+            door = r
 
         return ans
 
