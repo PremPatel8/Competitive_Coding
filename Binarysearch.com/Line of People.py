11ff48ceadaf56479646af08f754b3fe0685c3c5
Prem Patel <prempatel_8@outlook.com>
2021-07-20 14:28:46 -0500

    my sol

--- Binarysearch.com/Line of People.py
+++ Binarysearch.com/Line of People.py
@@ -2,9 +2,23 @@
 
 class Solution:
     def solve(self, n, a, b):
-        if b < a:
-            return b+1
-        elif b > a:
-            return a-1
+        b_end_point = b-1
+        a_start_point = n-a
+
+        # print(f"a_start_point = {a_start_point}")
+        # print(f"b_end_point = {b_end_point}")
+
+        if b_end_point < a_start_point:
+            return b_end_point+2
+        elif b_end_point >= a_start_point:
+            return a_start_point
         
         return -1
+
+
+n = 343914375
+a = 1
+b = 343914375
+
+myobj = Solution()
+print(myobj.solve(n,a,b))
