4463f228d243de074172711195b03f832325edb1
Prem Patel <prempatel_8@outlook.com>
2021-08-17 12:30:32 -0500

    problem URL and difficulty added

--- LeetCode/Search in Rotated Sorted Array.py
+++ LeetCode/Search in Rotated Sorted Array.py
@@ -6,6 +6,11 @@ Problem Name: Search in Rotated Sorted Array
 
 Problem Section: Sorting and Searching
 
+Problem URL: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Problem Difficulty: Medium
+
+
 Problem Statement:
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
