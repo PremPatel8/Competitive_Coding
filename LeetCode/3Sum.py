9ef8d0afbd274c91df2f8dd50d80b7be672147e2
Prem Patel <prempatel_8@outlook.com>
2021-08-17 12:44:48 -0500

    Problem Difficulty added

--- LeetCode/3Sum.py
+++ LeetCode/3Sum.py
@@ -8,6 +8,8 @@ Problem URL: https://leetcode.com/problems/3sum/
 
 Problem Section: Array, 2 pointers
 
+Problem Difficulty: Medium
+
 Problem Statement:
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
