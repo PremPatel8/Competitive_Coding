27c1aa723b462c8cd54ccde6359c77e1e6dbf7e6
Prem Patel <prempatel_8@outlook.com>
2021-08-17 11:30:00 -0500

    Problem Difficulty and URL added

--- LeetCode/Contains Duplicate.py
+++ LeetCode/Contains Duplicate.py
@@ -5,6 +5,10 @@ Problem Name: Contains Duplicate
 
 Problem Section: Array
 
+Problem Difficulty: Easy
+
+Problem URL: https://leetcode.com/problems/contains-duplicate/
+
 Problem Statement:
 Given an array of integers, find if the array contains any duplicates.
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
