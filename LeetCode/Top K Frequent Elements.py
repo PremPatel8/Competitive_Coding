eb285a1cbbfa487b8e7c6e97bd31b34489bd2aad
Prem Patel <prempatel_8@outlook.com>
2021-08-31 23:23:53 -0500

    Heap and Bucket Sort solution

--- LeetCode/Top K Frequent Elements.py
+++ LeetCode/Top K Frequent Elements.py
@@ -1,6 +1,7 @@
 from typing import List
 from collections import Counter
 import itertools
+import heapq
 
 """
 Problem Name: Top K Frequent Elements
@@ -30,23 +31,74 @@ Runtime: 124 ms
 Memory Usage: 18.3 MB """
 
 # Using bucket sort to get the elements sorted by increasing frequency (element with the highest frequency at the end)
+
+
 class Solution:
+    # Bucket Sort solution
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # O(1) time
         if k == len(nums) or len(nums) == 1:
             return nums
 
-        bucket = [[] for _ in range(len(nums) + 1)]
+        buckets = [[] for _ in range(len(nums) + 1)]
 
         Count = Counter(nums).items()
 
         for num, freq in Count:
-            bucket[freq].append(num)
+            buckets[freq].append(num)
 
-        # flat_list = [item for sublist in bucket for item in sublist]
-        flat_list = list(itertools.chain.from_iterable(bucket))
+        # print(buckets)
 
-        return flat_list[::-1][:k]
+        flat_list = []
+
+        # for bucket_list in buckets:
+        #     for num in bucket_list:
+        #         flat_list.append(num)
+
+        for bucket_list in reversed(buckets):
+            flat_list.extend(bucket_list)
+
+        # flat_list = [num for bucket_list in buckets for num in bucket_list]
+        # flat_list = list(itertools.chain.from_iterable(buckets))
+
+        # print(flat_list)
+
+        return flat_list[:k]
+
+    
+
+    # Heap solution
+    def topKFrequent_heap(self, nums: List[int], k: int) -> List[int]:
+        # O(1) time
+        if k == len(nums):
+            return nums
+
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)
+        print(count)
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        # return heapq.nlargest(k, count.keys(), key=count.get)
+
+        heap = []
+        heapq.heapify(heap)
+
+        for num, freq in count.items():
+            print(heap)
+            print((freq, num))
+            heapq.heappush(heap, (freq, num))
+            # Heap elements can be tuples. This is useful for assigning comparison values
+            # (such as task priorities) alongside the main record being tracked
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        print(heap)
+
+        res = [heapq.heappop(heap)[1] for _ in range(len(heap))]
+
+        return res
 
 
 myobj = Solution()
