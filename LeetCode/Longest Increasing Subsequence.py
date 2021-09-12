cbc9313f57ea89e13fc2f3066209068a3b9b0f76
Prem Patel <prempatel_8@outlook.com>
2021-09-12 17:14:22 -0500

    comments improved

--- LeetCode/Longest Increasing Subsequence.py
+++ LeetCode/Longest Increasing Subsequence.py
@@ -45,12 +45,16 @@ optimized search and replace using Binary Search
 
 # Approach 1: Dynamic Programming
 """ Algorithm
-Initialize an array dp with length nums.length and all elements equal to 1. dp[i] represents the length of the longest increasing subsequence that ends with the element at index i.
+Initialize an array dp with length nums.length and all elements equal to 1. 
+dp[i] represents the length of the longest increasing subsequence that ends with the element at index i.
 
 Iterate from i = 1 to i = nums.length - 1. At each iteration, use a second for loop to iterate from j = 0 to j = i - 1 (all the elements before i). 
 For each element before i, check if that element is smaller than nums[i]. If so, set dp[i] = max(dp[i], dp[j] + 1).
 
 Return the max value from dp.
+
+Time Complexity - O(n^2)
+Space Complexity - O(n)
  """
 
 """ 54 / 54 test cases passed.
@@ -72,24 +76,34 @@ class Solution:
 
 # Approach 2: Intelligently Build a Subsequence
 """
-As stated in the previous approach, the difficult part of this problem is deciding if an element is worth using or not. Consider the example `nums = [8, 1, 6, 2, 3, 10]`. Let's try to build an increasing subsequence starting with an empty one: `sub = []`.
+As stated in the previous approach, the difficult part of this problem is deciding if an element is worth using or not. Consider the example `nums = [8, 1, 6, 2, 3, 10]`. 
+Let's try to build an increasing subsequence starting with an empty one: `sub = []`.
 
 -   At the first element `8`, we might as well take it since it's better than nothing, so `sub = [8]`.
     
--   At the second element `1`, we can't increase the length of the subsequence since `8 >= 1`, so we have to choose only one element to keep. Well, this is an easy decision, let's take the `1` since there may be elements later on that are greater than `1` but less than `8`, now we have `sub = [1]`.
+-   At the second element `1`, we can't increase the length of the subsequence since `8 >= 1`, so we have to choose only one element to keep. 
+    Well, this is an easy decision, let's take the `1` since there may be elements later on that are greater than `1` but less than `8`, now we have `sub = [1]`.
     
 -   At the third element `6`, we can build on our subsequence since `6 > 1`, now `sub = [1, 6]`.
     
--   At the fourth element `2`, we can't build on our subsequence since `6 >= 2`, but can we improve on it for the future? Well, similar to the decision we made at the second element, if we replace the `6` with `2`, we will open the door to using elements that are greater than `2` but less than `6` in the future, so `sub = [1, 2]`.
+-   At the fourth element `2`, we can't build on our subsequence since `6 >= 2`, but can we improve on it for the future? Well, similar to the decision we made at the second element, 
+    if we replace the `6` with `2`, we will open the door to using elements that are greater than `2` but less than `6` in the future, so `sub = [1, 2]`.
     
--   At the fifth element `3`, we can build on our subsequence since `3 > 2`. Notice that this was only possible because of the swap we made in the previous step, so `sub = [1, 2, 3]`.
+-   At the fifth element `3`, we can build on our subsequence since `3 > 2`. Notice that this was only possible because of the swap we made in the previous step, 
+    so `sub = [1, 2, 3]`.
     
 -   At the last element `10`, we can build on our subsequence since `10 > 3`, giving a final subsequence `sub = [1, 2, 3, 10]`. The length of `sub` is our answer.
     
 
-It appears the best way to build an increasing subsequence is: for each element `num`, if `num` is greater than the largest element in our subsequence, then add it to the subsequence. Otherwise, perform a linear scan through the subsequence starting from the smallest element and replace the first element that is greater than or equal to `num` with `num`. This opens the door for elements that are greater than `num` but less than the element replaced to be included in the sequence.
+It appears the best way to build an increasing subsequence is: for each element `num`, if `num` is greater than the largest element in our subsequence, then add it to the subsequence. 
+Otherwise, perform a linear scan through the subsequence starting from the smallest element and replace the first element that is greater than or equal to `num` with `num`. 
+This opens the door for elements that are greater than `num` but less than the element replaced to be included in the sequence.
 
-One thing to add: this algorithm does not always generate a valid subsequence of the input, but the length of the subsequence will always equal the length of the longest increasing subsequence. For example, with the input `[3, 4, 5, 1]`, at the end we will have `sub = [1, 4, 5]`, which isn't a subsequence, but the length is still correct. The length remains correct because the length only changes when a new element is larger than any element in the subsequence. In that case, the element is appended to the subsequence instead of replacing an existing element. """
+One thing to add: this algorithm does not always generate a valid subsequence of the input, 
+but the length of the subsequence will always equal the length of the longest increasing subsequence. 
+For example, with the input `[3, 4, 5, 1]`, at the end we will have `sub = [1, 4, 5]`, which isn't a subsequence, 
+but the length is still correct. The length remains correct because the length only changes when a new element is larger than any element in the subsequence. 
+In that case, the element is appended to the subsequence instead of replacing an existing element. """
 
 """ 54 / 54 test cases passed.
 	Status: Accepted
@@ -97,7 +111,7 @@ Runtime: 92 ms
 Memory Usage: 14.6 MB """
 
 """ 
-Time complexity: O(N^2)
+Time complexity: O(N^2) only in the worst case
 
 This algorithm will have a runtime of O(N^2) only in the worst case. Consider an input where the first half is [1, 2, 3, 4, ..., 99998, 99999], then the second half is [99998, 99998, 99998, ..., 99998, 99998]. We would need to iterate (N/2)^2 times for the second half because there are N/2N / 2N/2 elements equal to 99998, and a linear scan for each one takes N/2N / 2N/2 iterations. This gives a time complexity of O(N^2).
 
@@ -134,7 +148,9 @@ class Solution:
 
 # Approach 3: Improve With Binary Search, Best Time Complexity, Run time
 """
-In the previous approach, when we have an element `num` that is not greater than all the elements in `sub`, we perform a linear scan to find the first element in `sub` that is greater than or equal to `num`. Since `sub` is in sorted order, we can use binary search instead to greatly improve the efficiency of our algorithm.
+In the previous approach, when we have an element `num` that is not greater than all the elements in `sub`, 
+we perform a linear scan to find the first element in `sub` that is greater than or equal to `num`. 
+Since `sub` is in sorted order, we can use binary search instead to greatly improve the efficiency of our algorithm.
 
 **Algorithm**
 
@@ -191,11 +207,15 @@ Memory Usage: 14.6 MB """
 
 
 class Solution:
-    def bisect_left_imp(self, a, x):
-        lo, hi = 0, len(a)
+    # Find the index of the first element in sub that is greater than or equal to num
+    def bisect_left_imp(self, sub, num):
+        lo = 0
+        # not len(sub)-1 because the num could need to be appened to the end of the list
+        hi = len(sub)
+
         while lo < hi:
             mid = lo + (hi - lo) // 2
-            if a[mid] < x:
+            if sub[mid] < num:
                 lo = mid + 1
             else:
                 hi = mid
