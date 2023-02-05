6a049236e840fdaba77d8c71ebac99f2a782279b
Ghos3t <prem.patel971@gmail.com>
2023-02-05 13:20:24 -0600

    Optimized iterative solution added

--- LeetCode/Merge Two Sorted Lists.py
+++ LeetCode/Merge Two Sorted Lists.py
@@ -15,6 +15,38 @@ Output: 1->1->2->3->4->4
 Resources:
 
 """
+
+""" 208 / 208 test cases passed.
+	Status: Accepted
+Runtime: 32 ms
+Memory Usage: 13.8 MB """
+
+# Solution techniques are
+# Time complexity : O(n) Space complexity : O(1) Optimized iterative solution
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        cur.next = l1 or l2
+
+        return dummy.next
+
+
 """ 208 / 208 test cases passed.
 	Status: Accepted
 Runtime: 34 ms
