693ba46e53ad25cc9d3e9c6e020a687074567e8a
Ghos3t <prem.patel971@gmail.com>
2023-10-23 01:41:49 -0500

    added Java sol and Python sol with fewer lines

--- LeetCode/Valid Parentheses.py
+++ LeetCode/Valid Parentheses.py
@@ -57,6 +57,46 @@ class Solution:
         return True if len(p_stack) == 0 else False
 
 
+# My Python Stack (Deque) and Dict sol with fewer lines of code
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_pair = {'(':')', '{':'}','[':']'}
+        stack = deque()
+        
+        for curr_bracket in s:
+            # If curr_bracket is a opening bracket
+            if curr_bracket in bracket_pair:
+                stack.append(curr_bracket)
+            # If curr_bracket is a closing bracket
+            elif len(stack) == 0 or bracket_pair[stack.pop()] != curr_bracket:
+                return False
+        
+        return len(stack) == 0
+    
+
+# My Java Stack and HashMap solution with fewer lives of code
+class Solution {
+    public boolean isValid(String s) {
+     Stack<Character> stack = new Stack<>();
+     HashMap<Character, Character> bracketPair = new HashMap<>();
+     bracketPair.put('(', ')');
+     bracketPair.put('{', '}');
+     bracketPair.put('[', ']');
+
+     for (char currBracket : s.toCharArray()) {
+         // currBracket a opening bracket
+         if (bracketPair.containsKey(currBracket)) {
+             stack.push(currBracket);
+         } else if (stack.size() <= 0 || !bracketPair.get(stack.pop()).equals(currBracket)) {
+             // currBracket a closing bracket
+             return false;
+         }
+     }
+
+     return stack.size() == 0;
+    }
+}
+
 myobj = Solution()
 # s = "()[]{}"
 # s = "([)]"
