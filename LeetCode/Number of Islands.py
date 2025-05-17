a977b9187db911d69f6f12bfe084a19958ecbab7
Ghos3t <prem.patel971@gmail.com>
2025-05-16 20:16:09 -0500

    description line length fix

--- LeetCode/Number of Islands.py
+++ LeetCode/Number of Islands.py
@@ -3,8 +3,9 @@ from typing import List
 
 """
 Number of Islands:
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is
-formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
 
 Example:
 Input: grid = [
--- structy/island count.py
+++ structy/island count.py
@@ -1,7 +1,11 @@
 """ island count
 https://structy.net/problems/island-count
 
-Write a function, island_count, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+Write a function, island_count, that takes in a grid containing Ws and Ls.
+W represents water and L represents land.
+The function should return the number of islands on the grid.
+An island is a vertically or horizontally connected region of land.
+
 test_00:
 
 grid = [
