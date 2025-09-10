f2ac4f57be55ebf7638d9cb8ec16baf4e96f1e5a
PremPatel8 <PremPatel_8@outlook.com>
2025-09-10 10:41:33 -0500

    Added 3 solutions for Longest Consecutive Sequence

--- LeetCode/Longest Consecutive Sequence.py
+++ LeetCode/Longest Consecutive Sequence.py
@@ -17,32 +17,68 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 
 Resources:
 """
-
-""" 68 / 68 test cases passed.
-	Status: Accepted
-Runtime: 60 ms
-Memory Usage: 15 MB """
-
-# Solution techniques are
-
-# Time complexity : O(n) Space complexity : O(n) Set Solution
-
-
+    
+    
+# Brute Force using Set
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        longest_streak = 0
+        res = 0
+        store = set(nums)
 
         for num in nums:
-            if num - 1 not in nums:
-                next_num = num + 1
+            streak, curr = 0, num
+            while curr in store:
+                streak += 1
+                curr += 1
+            res = max(res, streak)
+        return res
 
-                while next_num in nums:
-                    next_num += 1
+# Sorting
+# Time complexity : O(nlogn)
+# Space complexity : O(logn) or O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
 
-                longest_streak = max(longest_streak, next_num - num)
+        nums.sort()
 
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                if nums[i] == nums[i - 1] + 1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+    
+
+# Set
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest_streak = 0
+
+        for num in numSet:
+            # We check for num-1 because, ensuring that the number that would immediately precede the current number in a sequence is not present, 
+            # as that number would necessarily be part of a longer sequence.
+            if (num - 1) not in numSet:
+                current_streak = 1
+                while (num + current_streak) in numSet:
+                    current_streak += 1
+                
+                longest_streak = max(current_streak, longest_streak)
+                
         return longest_streak
+
 
 
 myobj = Solution()
