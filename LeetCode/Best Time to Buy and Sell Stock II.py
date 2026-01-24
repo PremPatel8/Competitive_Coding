c5c888e2ffde5e7d8b3279d82658854a4b430d44
PremPatel8 <PremPatel_8@outlook.com>
2026-01-23 19:27:05 -0600

    added comments

--- LeetCode/Best Time to Buy and Sell Stock II.py
+++ LeetCode/Best Time to Buy and Sell Stock II.py
@@ -68,41 +68,38 @@ print(myobj.maxProfit(inpt))
 """ 
 # Alt solution Peak Valley Approach
 
+Say the given array is:
+
+\[7,1,5,3,6,4\]
+
+If we plot the numbers of the given array on a graph, we get:
+
+![Profit Graph](https://leetcode.com/media/original_images/122_maxprofit_1.PNG)
+
 If we analyze the graph, we notice that the points of interest are the consecutive valleys and peaks.
 
-Mathematically speaking: TotalProfit=∑i(height(peaki)−height(valleyi)) Total Profit= \sum_{i}(height(peak_i)-height(valley_i)) TotalProfit=∑i​(height(peaki​)−height(valleyi​))
+Total Profit = ∑i(height(peaki)−height(valleyi))
 
-The key point is we need to consider every peak immediately following a valley to maximize the profit. In case we skip one of the peaks (trying to obtain more profit), we will end up losing the profit over one of the transactions leading to an overall lesser profit.
+The key point is we need to consider every peak immediately following a valley to maximize the profit.
+In case we skip one of the peaks (trying to obtain more profit), 
+we will end up losing the profit over one of the transactions leading to an overall lesser profit.
 
-For example, in the above case, if we skip peakipeak_ipeaki​ and valleyjvalley_jvalleyj​ trying to obtain more profit by considering points with more difference in heights, the net profit obtained will always be lesser than the one obtained by including them, since CCC will always be lesser than A+BA+BA+B.
-
-class Solution {
-    public int maxProfit(int[] prices) {
-        int i = 0;
-        int valley = prices[0];
-        int peak = prices[0];
-        int maxprofit = 0;
-        while (i < prices.length - 1) {
-            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
-                i++;
-            valley = prices[i];
-            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
-                i++;
-            peak = prices[i];
-            maxprofit += peak - valley;
-        }
-        return maxprofit;
-    }
-}
-
-class Solution {
-    public int maxProfit(int[] prices) {
-        int maxprofit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1])
-                maxprofit += prices[i] - prices[i - 1];
-        }
-        return maxprofit;
-    }
-}
+For example, in the above case, if we skip peaki and valleyj trying to obtain more profit by considering points with more difference in heights,
+the net profit obtained will always be lesser than the one obtained by including them, since C will always be lesser than A+B.
 """
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0
+        valley = prices[0]
+        peak = prices[0]
+        maxprofit = 0
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            valley = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            peak = prices[i]
+            maxprofit += peak - valley
+        return maxprofit
